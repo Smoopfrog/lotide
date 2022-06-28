d198f2ecfcb31c7275d3e668d2c1461e1abf1384
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-06-28 02:01:10 +0000

    Adding tail function to repo

---
+++ tail.js
@@ -0,0 +1,25 @@
+// Assert Equal function
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🔥🔥🔥 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Tail
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+//Test
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail([]));
+console.log(tail([1]));
