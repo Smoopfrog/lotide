239a023e28852e63e4e33d1e5f42c852036820de
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-06-28 00:31:48 +0000

    first edition of assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🔥🔥🔥 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😡😡😡 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2, 1);
+assertEqual(1, 1);
+assertEqual("Okay", "Okay");
