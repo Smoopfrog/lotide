9d8a7b8784953612c2d94361ac2c80db1f319ab3
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-06-28 01:46:06 +0000

    Created head function

---
+++ head.js
@@ -0,0 +1,19 @@
+//Assert Equal function
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🔥🔥🔥 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Head function
+const head = function(array) {
+  let headVal = array[0];
+  return headVal;
+};
+
+//Tests
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
