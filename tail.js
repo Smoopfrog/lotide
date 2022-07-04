ce8540c1aea26d46bdeafb24b493c11b291a69b1
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-04 21:42:03 +0000

    refactored

--- tail.js
+++ tail.js
@@ -12,9 +12,3 @@ const tail = function(array) {
 
 module.exports = tail;
 
-//Test
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail([]));
-console.log(tail([1]));
