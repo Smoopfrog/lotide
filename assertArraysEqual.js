b7a36d7d32fe9b0835e1e6b30bed6047ab869d3e
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-04 21:41:32 +0000

    exported assertArraysEqual function and initial commit of assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-// Checks for matching arrays
-const eqArrays = function(arrayOne, arrayTwo) {
-  let match = true;
-  // Check to see if the arrays are the same length
-  if (arrayOne.length !== arrayTwo.length) {
-    match = false;
-  }
-  // Check to if the elements are the same
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      match = false;
-    }
-  }
-  return match;
-};
+const eqArrays = require('./eqArrays')
 
 // Prints a test for matching arrays
 const assertArraysEqual = function(arrayOne, arrayTwo) {
@@ -24,5 +10,4 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
+module.exports = assertArraysEqual;
